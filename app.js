f76064efb20019544bfde4c2e053a6dddb2eb9a8
mgamamunbd <mgamamunbd@gmail.com>
2022-02-11 17:22:20 +0400

    changed

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-// const allButton = document.getElementsByClassName("button-test");
-// const prime = document.getElementById("prime");
-
-// console.log(allButton);
-// // console.log(prime);
-
-// for (const button of allButton) {
-//   button.addEventListener("click", function (e) {
-//     // console.log(e.target.parentNode);
-//       console.log(e);
-//   });
-// }
+/** @format */
 
 let count = 0;
 function add(e) {
@@ -23,20 +12,3 @@ function minus(params) {
 function control(id) {
   document.getElementById(id).innerText = count;
 }
-
-// function add(params) {
-//   bro();
-// }
-// add();
-// function minus(params) {
-//   bro();
-// }
-
-// function bro(params) {
-//   console.log("brooooo");
-// }
-
-
-
-
-
